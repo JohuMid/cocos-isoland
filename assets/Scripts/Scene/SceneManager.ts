b22115c7de1786911e592d12fea18959cec1a04e
JohuMid <johumid@outlook.com>
2025-07-04 17:01:51 +0800

    feat: 添加场景管理器和场景枚举，更新资源版本和配置。

---
+++ assets/Scripts/Scene/SceneManager.ts
@@ -0,0 +1,19 @@
+import { _decorator, Component, director, Node } from 'cc';
+import { SceneEnum } from '../../Enum';
+const { ccclass, property } = _decorator;
+
+@ccclass('SceneManager')
+export class SceneManager extends Component {
+    protected start(): void {
+        director.preloadScene(SceneEnum.H1);
+        director.preloadScene(SceneEnum.H2);
+        director.preloadScene(SceneEnum.H3);
+        director.preloadScene(SceneEnum.H4);
+    }
+
+    changeScene(e: Event, scene: string) {
+        director.loadScene(scene as SceneEnum);
+    }
+}
+
+
